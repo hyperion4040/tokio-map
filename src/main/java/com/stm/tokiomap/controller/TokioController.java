139cbd041503997fdd2ebe860a3b80c6dec779e6
Adrian Kozłowski <adrian.kozlowski2611@interia.pl>
2020-12-06 12:17:18 +0100

    Added coordinates validation

--- src/main/java/com/stm/tokiomap/controller/TokioController.java
+++ src/main/java/com/stm/tokiomap/controller/TokioController.java
@@ -13,6 +13,7 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Objects;
 
 @Controller
 public class TokioController {
@@ -25,13 +26,14 @@ public class TokioController {
         InputStream in = getClass()
                 .getResourceAsStream("/tokio.png");
         final ClassLoader loader = getClass().getClassLoader();
-        BufferedImage image = cropImage(IOUtils.toByteArray(in),x1,y1,x2,y2);
-        File pathFile = new File(loader.getResource(".").getFile()+"tokio1.png");
+        BufferedImage image = cropImage(IOUtils.toByteArray(in),y1,x1,y2,x2);
+        File pathFile = new File(Objects.requireNonNull(loader.getResource(".")).getFile()+"tokio1.png");
         ImageIO.write(image,"png", pathFile);
 
         InputStream out = loader.getResourceAsStream("tokio1.png");
 
-        return IOUtils.toByteArray(out);
+        return IOUtils.toByteArray(Objects.requireNonNull(out));
+
     }
 
     public static BufferedImage cropImage(byte[] image, int y1, int x1,int y2,int x2) throws IOException {
@@ -39,11 +41,15 @@ public class TokioController {
         BufferedImage originalImage = ImageIO.read(in);
 //        int x = originalImage.getHeight()/2;
 //        int y = originalImage.getHeight()/2;
-        int weight = x2 - x1;
-        int height = y2 - y1;
-        int x = weight/2;
-        int y = height/2;
-        return originalImage.getSubimage(x, y, x2 - x1, y2 -y1);
+        int xMax = Math.min(Integer.max(x2, x1), 500);
+        int xMin = Math.max(Integer.min(x1,x2),0);
+        int weight = xMax - xMin;
+        int yMax = Math.min(Integer.max(y2, y1), 500);
+        int yMin = Math.max(Integer.min(y1,y2),0);
+        int height = yMax - yMin;
+        int x = weight/2 + xMin;
+        int y = height/2 + yMin;
+        return originalImage.getSubimage(x, y, weight, height);
     }
 
 
